4bfdb925257f10a289cdfdf0fa280c2e487c8147
Forhad Hossen <forhadhossenbabu@gmail.com>
2026-01-01 12:41:42 +0600

    initail build

---
+++ .sanity/runtime/app.js
@@ -0,0 +1,10 @@
+
+// This file is auto-generated on 'sanity dev'
+// Modifications to this file are automatically discarded
+import {createRoot} from 'react-dom/client'
+import {createElement} from 'react'
+import App from "/Users/fhb/DevProjects/ecommerce-ai/app/(admin)/admin/page.tsx"
+
+const root = createRoot(document.getElementById('root'))
+const element = createElement(App)
+root.render(element)
---
+++ sanity.cli.ts
@@ -0,0 +1,24 @@
+/**
+ * This configuration file lets you run `$ sanity [command]` in this folder
+ * Go to https://www.sanity.io/docs/cli to learn more.
+ **/
+import { defineCliConfig } from "sanity/cli";
+import "dotenv/config";
+
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
+const organizationId = process.env.NEXT_PUBLIC_SANITY_ORG_ID;
+
+if (!organizationId || !projectId || !dataset) {
+  throw new Error(
+    "NEXT_PUBLIC_SANITY_ORG_ID, NEXT_PUBLIC_SANITY_PROJECT_ID, or NEXT_PUBLIC_SANITY_DATASET is not set"
+  );
+}
+
+export default defineCliConfig({
+  app: {
+    organizationId,
+    entry: "./app/(admin)/admin/page.tsx",
+  },
+  api: { projectId, dataset },
+});
