e5d3b6ac536c9b8062a873d390d5a5ee6715add7
Forhad Hossen <forhadhossenbabu@gmail.com>
2026-01-01 23:51:27 +0600

    sanity sdk issue

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -2,8 +2,11 @@
  * This configuration file lets you run `$ sanity [command]` in this folder
  * Go to https://www.sanity.io/docs/cli to learn more.
  **/
+import { fileURLToPath } from "node:url";
 import { defineCliConfig } from "sanity/cli";
 import "dotenv/config";
+
+const rootDir = fileURLToPath(new URL(".", import.meta.url));
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
@@ -19,6 +22,13 @@ export default defineCliConfig({
   app: {
     organizationId,
     entry: "./app/(admin)/admin/page.tsx",
+  },
+  vite: {
+    resolve: {
+      alias: {
+        "@": rootDir,
+      },
+    },
   },
   api: { projectId, dataset },
 });
