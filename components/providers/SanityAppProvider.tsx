46ee6954901d3100a9e5ab74eb9e77bc66811370
Forhad Hossen <forhadhossenbabu@gmail.com>
2026-01-05 23:47:37 +0600

    fixed admin issue

--- components/providers/SanityAppProvider.tsx
+++ components/providers/SanityAppProvider.tsx
@@ -9,14 +9,18 @@ const readToken =
   process.env.NEXT_PUBLIC_SANITY_READ_TOKEN;
 
 function SanityAppProvider({ children }: { children: React.ReactNode }) {
+  const apiHost = projectId ? `${projectId}.api.sanity.io` : undefined;
+
   return (
     <ResourceProvider
       projectId={projectId}
       dataset={dataset}
+      studioMode={{ enabled: true }}
       auth={
         readToken
           ? {
               token: readToken,
+              apiHost,
               clientFactory: (config) =>
                 createClient({
                   ...config,
